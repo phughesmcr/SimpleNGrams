2dbbe2455b08d5358d2b83431b111d5ed0823337
phugh <peter@phugh.es>
2019-11-16 18:21:32 +0000

    1.0.0
    + added async and callback functionality
    + added ability to normalize strings
    + tweaked default options
    + improved documentation

--- README.md
+++ README.md
@@ -2,30 +2,67 @@
 
 The easiest way to get an array of n-grams from a string!
 
-## Useage
+## Usage
+
+### Asynchronous
 ```javascript
 const sng = require('simplengrams');
 const text = 'A string of text...';
-const bigrams = sng(text, 2);
-const trigrams = sng(text, 3);
-console.log(bigrams, trigrams);
+const opts = {logs: 3, strict: false};
+const bigrams = await sng.ngrams(text, 2, opts);
+console.log(bigrams);
 ```
 
-Errors return an empty two-dimensional array, i.e. [[]]
+Or, with `.then()` and `.catch()`:
 
-If the n-gram size (i.e. '2' or '3' in the example above) is greater than the length of the input string, an empty two-dimensional array is returned, i.e. [[]].
+```javascript
+const sng = require('simplengrams');
+const text = 'A string of text...';
+const opts = {logs: 3, strict: false};
+sng.ngrams(text, 3, opts)
+  .then((arr) => {
+    console.log(trigrams);
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
+
+```
+
+### Synchronous
+```javascript
+const sng = require('simplengrams');
+const text = 'A string of text...';
+const opts = {logs: 3, strict: false};
+const bigrams = sng.ngramsSync(text, 2, opts);
+console.log(bigrams);
+```
+
+Errors return an empty two-dimensional array, i.e. `[[]]`
+
+If the n-gram size (i.e. '2' in the example above) is greater than the length of the input string, an empty two-dimensional array is returned, i.e. `[[]]`.
+
+### Callback
+```javascript
+const sng = require('simplengrams');
+const text = 'A string of text...';
+const opts = {logs: 3, strict: false};
+ngramsSync(text, 2, opts, function(err, bigrams) {
+  console.log(bigrams);
+});
+```
 
 ## Example
 ### Input
 ```javascript
 const sng = require('simplengrams');
 const text = 'In the beginning God created the heavens and the earth.';
-const opts = {logs: 3};
+const opts = {logs: 3, strict: false};
 const bigrams = sng(text, 2, opts);
 console.log(bigrams);
 ```
 
-### Bigram Output
+### Output
 ```javascript
 [
   [ 'In', 'the' ],
@@ -45,19 +82,34 @@ console.log(bigrams);
 The options object is optional, the defaults are:
 ```
 {
-  logs: 3
+  logs: 3,
+  normalize: true,
+  strict: false
 }
 ```
 
-### 'logs'
+### logs
 **Number - valid options: 0, 1, 2, 3 (default)**
+
 Used to control console.log, console.warn, and console.error outputs.
 * 0 = suppress all logs
 * 1 = print errors only
 * 2 = print errors and warnings
 * 3 = print all console logs
 
+### normalize
+**boolean - valid options: `true` (default), or `false`**
+
+[Normalise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize) strings. E.g. when set to `true`, 'mañana' become 'manana'.
+
+### strict
+**Boolean - valid options: true, false (default)**
+
+When `strict` is set to `true` functions `throw` errors.
+* `false` = functions return empty arrays on error
+* `true`  = functions `throw` on error
+
 ## License
-(C) 2017-18 [P. Hughes](https://www.phugh.es). All rights reserved.
+(C) 2017-19 [P. Hughes](https://www.phugh.es). All rights reserved.
 
 Shared under the [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/) license.
--- index.js
+++ index.js
@@ -1,108 +1,167 @@
 /**
- * SimpleNgrams
- * v0.3.3
+ * @preserve
+ * SimpleNGrams
+ * v1.0.0
  *
  * Help me make this better:
  * https://github.com/phugh/simplengrams
  *
- * (C) 2017-18 P. Hughes
+ * (C) 2017-19 P. Hughes
  * License : Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
  *
- * Usage example:
- * const ngrams = require('simplengrams');
- * const txt = 'A string of text, any text what so ever!';
- * const opts = {logs: 3};
- * const bigrams = ngrams(txt, 2, opts);
- * const trigrams = ngrams(txt, 3, opts);
- * console.log(bigrams, trigrams);
+ * @name         SimpleNGrams
+ * @file         index.js
+ * @description  Returns a 2d array of n-grams
+ * @version      1.0.0
+ * @exports      (simplengrams|simplengramsSync)
+ * @requires     happynodetokenizer
+ * @author       P. Hughes <peter@phugh.es> (https://www.phugh.es)
+ * @copyright    2017-19 P. Hughes. All rights reserved.
+ * @license      CC-BY-NC-SA-3.0
+ *
+ * @example
+ *  const ngrams = require('simplengrams');
+ *  const txt = 'A string of text, any text what so ever!';
+ *  const opts = {logs: 3, normalize: true, strict: true};
+ *  const bigrams = ngrams(txt, 2, opts);
+ *  const trigrams = ngrams(txt, 3, opts);
+ *  console.log(bigrams, trigrams);
  *
  * Options: {
  *    "logs": 3,          // 0 = suppress all logs
  *                        // 1 = print errors only
  *                        // 2 = print errors and warnings
  *                        // 3 = print all console logs
+ *    "normalize": true,  // false = don't normalize string
+ *                        // true = apply string.prototype.normalize()
+ *    "strict": false,    // false = don't throw errors
+ *                        // true  = throw errors
  * }
- *
- * @param {string} str    input string
- * @param {Number} [n=2]  size of ngrams - unigrams = 1, bigrams = 2 etc.
- * @param {Object} [opts] options object
- * @return {Array} array of n-grams
  */
 
-(function() {
+(() => {
   'use strict';
   const tokenizer = require('happynodetokenizer');
 
   /**
-   * @function getNgrams
-   * @param {Array} arr  array of tokens
+   * @function _getter
+   * @private
+   * @param {Array} tokens  array of tokens
    * @param {Number} n   size of n-grams
    * @return {Array} array of n-grams
    */
-  function getNgrams(arr, n) {
-    const ngrams = [];
-
-    /**
-     * @function mainLoop
-     * @param  {Number} i
-     * @return {Array} array of n-grams
-     */
-    function mainLoop(i) {
-      const a = [];
-      for (let h = 0; h < n; h++) {
-        a.push(arr[(i + n) + (h - n)]);
-      }
-      return a;
-    }
-
-    const len = arr.length - n + 1;
+  const _getter = (tokens, n) => {
+    const output = [];
+    const len = (tokens.length - n) + 1;
     let i = 0;
-    for (i = 0; i < len; i++) {
-      ngrams.push(mainLoop(i));
+    for (i; i < len; i++) {
+      const grams = [];
+      let j = 0;
+      for (j; j < n; j++) {
+        grams.push(tokens[(i + n) + (j - n)]);
+      }
+      output.push(grams);
     }
-    return ngrams;
-  }
+    return output;
+  };
 
   /**
-   * @function simplengrams
+   * @function ngrams
+   * @public
+   * @async
    * @param {string} str    input string
    * @param {Number} [n=2]  n-gram size
    * @param {Object} [opts] options object
+   * @return {Promise} array of n-grams
+   */
+  const ngrams = async (str, n = 2, opts = {}) => {
+    return ngramsSync(str, n, opts, async (err, data) => {
+      if (err) throw new Error(err);
+      return data;
+    });
+  };
+
+  /**
+   * @function ngramsSync
+   * @public
+   * @param {string} str    input string
+   * @param {Number} [n=2]  n-gram size
+   * @param {Object} [opts={}] options object
+   * @param {function} [cb] callback function
    * @return {Array} array of n-grams
    */
-  function simplengrams(str, n = 2, opts = {}) {
+  const ngramsSync = (str, n = 2, opts = {}, cb = undefined) => {
     // error handling
     if (!str) {
-      if (opts.logs > 0) console.error('simpleNGrams needs input! Returning empty 2d array: [[]].');
-      return [[]];
+      if (opts.strict) {
+        throw new Error('simpleNGrams: no valid input found.');
+      } else {
+        if (opts.logs > 0) console.error('simpleNGrams: no valid input found. Returning empty 2d array: [[]].');
+        return [[]];
+      }
     }
     // default options
-    opts.logs = (typeof opts.logs === 'number') ? opts.logs : 3;
-    n = (typeof n === 'undefined') ? 2 : n; // default to bigrams
-    n = (typeof n === 'number') ? n : Number(n);
     str = (typeof str === 'string') ? str : str.toString();
+    n = (typeof n !== 'undefined' && typeof n === 'number') ? n : 2; // default to bigrams
+    opts.logs = (typeof opts.logs !== 'undefined' && typeof opts.logs === 'number') ? opts.logs : 3;
+    opts.normalize = (typeof opts.normalize !== 'undefined' && typeof opts.normalize === 'boolean') ? opts.normalize : true;
+    opts.strict = (typeof opts.strict !== 'undefined' && typeof opts.strict === 'boolean') ? opts.strict : false;
     // tokenize!
-    const tokens = tokenizer(str, {logs: opts.logs});
+    const tokens = tokenizer.tokenizeSync(str, {logs: opts.logs, normalize: opts.normalize, strict: opts.strict});
     if (!tokens) {
-      if (opts.logs > 1) console.warn('simpleNGrams found no tokens. Returning empty 2d array: [[]].');
-      return [[]];
+      if (opts.strict) {
+        if (cb && typeof cb === 'function') {
+          return cb('simpleNGrams: no tokens returned from input.');
+        } else {
+          throw new Error('simpleNGrams: no tokens returned from input.');
+        }
+      } else {
+        if (opts.logs > 1) console.warn('simpleNGrams: no tokens returned from input. Returning empty 2d array: [[]].');
+        if (cb && typeof cb === 'function') {
+          return cb(null, [[]]);
+        } else {
+          return [[]];
+        }
+      }
     }
-    // return empty if n > tokens
+    // handle n > tokens
     if (n > tokens.length && opts.logs > 1) {
-      console.warn('simpleNgrams: \'n\' is greater than length of text. Returning empty 2d array: [[]]');
-      return [[]];
+      if (opts.strict) {
+        if (cb && typeof cb === 'function') {
+          return cb('simpleNGrams: \'n\' is greater than length of input.');
+        } else {
+          throw new Error('simpleNGrams: \'n\' is greater than length of input.');
+        }
+      } else {
+        if (opts.logs > 1) console.warn('simpleNGrams: \'n\' is greater than length of input. Returning empty 2d array: [[]]');
+        if (cb && typeof cb === 'function') {
+          return cb(null, [[]]);
+        } else {
+          return [[]];
+        }
+      }
     }
-    // return n-gram arrays!
-    return getNgrams(tokens, n);
-  }
+    // return n-gram arrays
+    if (cb && typeof cb === 'function') {
+      return cb(null, _getter(tokens, n));
+    } else {
+      return _getter(tokens, n);
+    }
+  };
 
+  // export!
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = simplengrams;
+      exports = module.exports = {
+        ngrams: ngrams,
+        ngramsSync: ngramsSync,
+      };
     }
-    exports.simplengrams = simplengrams;
+    exports.ngrams = ngrams;
+    exports.ngramsSync = ngramsSync;
   } else {
-    global.simplengrams = simplengrams;
+    global.ngrams = ngrams;
+    global.ngramsSync = ngramsSync;
   }
 })();
