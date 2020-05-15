fcb08c4efd596e077c054f98a4dc1e0f5ed2d815
phugh <peter@phugh.es>
2020-05-15 14:46:22 +0100

    Rewrite - Simplified module - Input now allows strings or array of strings - All options replaced - Allow for custom padding and splitting options - Improved documentation

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
 /**
  * @preserve
  * SimpleNGrams
- * v1.0.0
+ * v2.0.0
  *
  * Help me make this better:
  * https://github.com/phugh/simplengrams
  *
- * (C) 2017-19 P. Hughes
+ * (C) 2017-20 P. Hughes
  * License : Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
  *
  * @name         SimpleNGrams
  * @file         index.js
  * @description  Returns a 2d array of n-grams
- * @version      1.0.0
- * @exports      (simplengrams|simplengramsSync)
+ * @version      2.0.0
+ * @exports      (from|fromSync)
  * @requires     happynodetokenizer
  * @author       P. Hughes <peter@phugh.es> (https://www.phugh.es)
- * @copyright    2017-19 P. Hughes. All rights reserved.
+ * @copyright    2017-20 P. Hughes. All rights reserved.
  * @license      CC-BY-NC-SA-3.0
  *
  * @example
  *  const ngrams = require('simplengrams');
  *  const txt = 'A string of text, any text what so ever!';
- *  const opts = {logs: 3, normalize: true, strict: true};
- *  const bigrams = ngrams(txt, 2, opts);
- *  const trigrams = ngrams(txt, 3, opts);
+ *  const bigrams = await ngrams.from(txt);
+ *  const trigrams = ngrams.fromSync(txt, 3);
  *  console.log(bigrams, trigrams);
- *
- * Options: {
- *    "logs": 3,          // 0 = suppress all logs
- *                        // 1 = print errors only
- *                        // 2 = print errors and warnings
- *                        // 3 = print all console logs
- *    "normalize": true,  // false = don't normalize string
- *                        // true = apply string.prototype.normalize()
- *    "strict": false,    // false = don't throw errors
- *                        // true  = throw errors
- * }
  */
 
-(() => {
+// eslint-disable-next-line no-extra-semi
+;(() => {
   'use strict';
-  const tokenizer = require('happynodetokenizer');
 
   /**
-   * @function _getter
+   * @function _splitter
    * @private
-   * @param {Array} tokens  array of tokens
-   * @param {Number} n   size of n-grams
-   * @return {Array} array of n-grams
+   * @param {Array<string>} tokens array of strings
+   * @param {number} n gram size
+   * @param {boolean | Array<any>} pad add padding to start and end of output
+   * @return {Array<Array<string>>} array of n-grams
    */
-  const _getter = (tokens, n) => {
+  const _splitter = (tokens, n, pad) => {
     const output = [];
-    const len = (tokens.length - n) + 1;
-    let i = 0;
-    for (i; i < len; i++) {
+    const padIsArray = Array.isArray(pad);
+    if (pad === true || padIsArray) {
+      let start = null;
+      let end = null;
+      if (padIsArray) {
+        if (pad.length === 2) {
+          start = pad[0];
+          end = pad[1];
+        } else if (pad.length === 1) {
+          start = end = pad[0];
+        } else {
+          throw new Error(`Wrong number of elements in pad array. Expected 1 or 2, found ${pad.length}.`);
+        }
+      }
+      for (let i = 1; i < n; i++) {
+        if (start !== undefined) tokens.unshift(start);
+        if (end !== undefined) tokens.push(end);
+      }
+    }
+    const len = tokens.length - (n + 1);
+    for (let i = 0; i < len; i++) {
       const grams = [];
-      let j = 0;
-      for (j; j < n; j++) {
-        grams.push(tokens[(i + n) + (j - n)]);
+      for (let j = 0; j < n; j++) {
+        grams.push(tokens[i + n + (j - n)]);
       }
       output.push(grams);
     }
@@ -67,101 +73,75 @@
   };
 
   /**
-   * @function ngrams
-   * @public
-   * @async
-   * @param {string} str    input string
-   * @param {Number} [n=2]  n-gram size
-   * @param {Object} [opts] options object
-   * @return {Promise} array of n-grams
+   * @function _validateInput
+   * @private
+   * @param {string | Array<string>} input
+   * @return {boolean}
    */
-  const ngrams = async (str, n = 2, opts = {}) => {
-    return ngramsSync(str, n, opts, async (err, data) => {
-      if (err) throw new Error(err);
-      return data;
-    });
+  const _validateInput = (input) => {
+    if (
+      !input ||
+      (!Array.isArray(input) && typeof input !== 'string') ||
+      (typeof input === 'string' && !input.trim()) ||
+      (Array.isArray(input) && input.length === 0) ||
+      (Array.isArray(input) && (input.filter((x) => typeof x !== 'string').length > 0))
+    ) {
+      return false;
+    } else {
+      return true;
+    }
   };
 
   /**
-   * @function ngramsSync
+   * @function fromSync
    * @public
-   * @param {string} str    input string
-   * @param {Number} [n=2]  n-gram size
-   * @param {Object} [opts={}] options object
-   * @param {function} [cb] callback function
-   * @return {Array} array of n-grams
+   * @param {string | Array<string>} input non-empty string or array of strings
+   * @param {number} [n=2] gram size - defaults to bigrams (n=2)
+   * @param {boolean | Array<any>} [pad] pad start and end of output?
+   * @param {string | RegExp} [splitPattern] pattern used to split strings into tokens - defaults to spaces
+   * @return {Array<Array<string>>}
    */
-  const ngramsSync = (str, n = 2, opts = {}, cb = undefined) => {
-    // error handling
-    if (!str) {
-      if (opts.strict) {
-        throw new Error('simpleNGrams: no valid input found.');
-      } else {
-        if (opts.logs > 0) console.error('simpleNGrams: no valid input found. Returning empty 2d array: [[]].');
-        return [[]];
-      }
-    }
-    // default options
-    str = (typeof str === 'string') ? str : str.toString();
-    n = (typeof n !== 'undefined' && typeof n === 'number') ? n : 2; // default to bigrams
-    opts.logs = (typeof opts.logs !== 'undefined' && typeof opts.logs === 'number') ? opts.logs : 3;
-    opts.normalize = (typeof opts.normalize !== 'undefined' && typeof opts.normalize === 'boolean') ? opts.normalize : true;
-    opts.strict = (typeof opts.strict !== 'undefined' && typeof opts.strict === 'boolean') ? opts.strict : false;
-    // tokenize!
-    const tokens = tokenizer.tokenizeSync(str, {logs: opts.logs, normalize: opts.normalize, strict: opts.strict});
-    if (!tokens) {
-      if (opts.strict) {
-        if (cb && typeof cb === 'function') {
-          return cb('simpleNGrams: no tokens returned from input.');
-        } else {
-          throw new Error('simpleNGrams: no tokens returned from input.');
-        }
-      } else {
-        if (opts.logs > 1) console.warn('simpleNGrams: no tokens returned from input. Returning empty 2d array: [[]].');
-        if (cb && typeof cb === 'function') {
-          return cb(null, [[]]);
-        } else {
-          return [[]];
-        }
-      }
-    }
-    // handle n > tokens
-    if (n > tokens.length && opts.logs > 1) {
-      if (opts.strict) {
-        if (cb && typeof cb === 'function') {
-          return cb('simpleNGrams: \'n\' is greater than length of input.');
-        } else {
-          throw new Error('simpleNGrams: \'n\' is greater than length of input.');
-        }
-      } else {
-        if (opts.logs > 1) console.warn('simpleNGrams: \'n\' is greater than length of input. Returning empty 2d array: [[]]');
-        if (cb && typeof cb === 'function') {
-          return cb(null, [[]]);
-        } else {
-          return [[]];
-        }
-      }
-    }
-    // return n-gram arrays
-    if (cb && typeof cb === 'function') {
-      return cb(null, _getter(tokens, n));
+  const fromSync = (input, n = 2, pad = false, splitPattern = ' ') => {
+    if (!_validateInput(input)) {
+      throw new TypeError(`No valid input found. Expected non-empty string or array of non-empty string, found ${typeof input}.`);
     } else {
-      return _getter(tokens, n);
+      let tokens;
+      if (typeof input === 'string') {
+        tokens = input.split(splitPattern);
+      } else {
+        tokens = input;
+      }
+      if (n > tokens.length && pad === false) {
+        return [[...tokens]];
+      } else {
+        return _splitter(tokens, n, pad);
+      }
     }
+  };
+
+  /**
+   * @function from
+   * @public
+   * @async
+   * @param {string | Array<string>} input non-empty string or array of strings
+   * @param {number} [n=2] gram size - defaults to bigrams (n=2)
+   * @param {boolean | Array<any>} [pad] pad start and end of output?
+   * @param {string | RegExp} [splitPattern] pattern used to split strings into tokens - defaults to spaces
+   * @return {Promise<Array<Array<string>>>}
+   */
+  const from = async (input, n = 2, pad = false, splitPattern = ' ') => {
+    return fromSync(input, n, pad, splitPattern);
   };
 
   // export!
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = {
-        ngrams: ngrams,
-        ngramsSync: ngramsSync,
+        from: from,
+        fromSync: fromSync,
       };
     }
-    exports.ngrams = ngrams;
-    exports.ngramsSync = ngramsSync;
-  } else {
-    global.ngrams = ngrams;
-    global.ngramsSync = ngramsSync;
+    exports.from = from;
+    exports.fromSync = fromSync;
   }
 })();
