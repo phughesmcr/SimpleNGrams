b568982901ff654e4179b429678f950c52961161
phugh <peter@phugh.es>
2020-05-27 15:22:25 +0100

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export function from(input: string | Array<string>, n: number, pad: boolean | Array<any>, splitPattern: string | RegExp): Promise<Array<Array<string>>>;
+export function fromSync(input: string | Array<string>, n: number, pad: boolean | Array<any>, splitPattern: string | RegExp): Array<Array<string>>;
